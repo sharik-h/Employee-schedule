6ce60fe42019b14af68e3d3cc2c37ad8dd5e1b0c
Mohammed sharikh <mohammedsharik0123@gmail.com>
2023-02-17 23:19:32 +0530

    Created a simple otp authentication  page UI this page verify the entered otp

---
+++ app/src/main/java/com/example/employeesheduler/LoginPages/authPage.kt
@@ -0,0 +1,60 @@
+package com.example.employeesheduler.LoginPages
+
+import android.widget.Space
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Preview(showBackground = true)
+@Composable
+fun authPage() {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(20.dp)
+    ) {
+
+        var otp by remember { mutableStateOf("") }
+
+        Text(
+            text = "Verify OTP",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Blue,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.height(25.dp))
+        Text(text = "Enter the otp sended to you number.", color = Color.Gray)
+        OutlinedTextField(
+            value = otp,
+            onValueChange = { otp = it },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            colors = TextFieldDefaults.textFieldColors(
+                unfocusedIndicatorColor = Color.Transparent,
+                focusedIndicatorColor = Color.Blue,
+                backgroundColor = Color.LightGray,
+                cursorColor = Color.Blue
+            )
+        )
+        Spacer(modifier = Modifier.height(25.dp))
+        Button(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(Color.Blue),
+            modifier = Modifier
+                .fillMaxWidth()
+                .size(55.dp)
+        ) {
+            Text(text = "Verify", color = Color.White)
+        }
+    }
+}
